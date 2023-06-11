ec66927fd7e55aeecef248a77027c34303feba98
IkeVoodoo <55694349+IkeVoodoo@users.noreply.github.com>
2023-06-11 12:47:23 +0200

    Setup item use listener and add it to item listeners

--- spigot-core-items/src/main/java/me/ikevoodoo/spigotcore/items/listeners/ItemListeners.java
+++ spigot-core-items/src/main/java/me/ikevoodoo/spigotcore/items/listeners/ItemListeners.java
@@ -1,9 +1,19 @@
 package me.ikevoodoo.spigotcore.items.listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
 public final class ItemListeners {
 
     private ItemListeners() {
         throw new IllegalStateException("Did you really just attempt this? The constructor is private for a reason you dingus.");
+    }
+
+    public static void registerAllListeners(@NotNull Plugin plugin) {
+        var pluginManager = Bukkit.getPluginManager();
+
+        pluginManager.registerEvents(new ItemUseListener(),  plugin);
     }
 
 }
---
+++ spigot-core-items/src/main/java/me/ikevoodoo/spigotcore/items/listeners/ItemUseListener.java
@@ -0,0 +1,17 @@
+package me.ikevoodoo.spigotcore.items.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class ItemUseListener implements Listener {
+
+    protected ItemUseListener() {
+
+    }
+
+    @EventHandler
+    private void onPlayerInteract(PlayerInteractEvent event) {
+
+    }
+}
