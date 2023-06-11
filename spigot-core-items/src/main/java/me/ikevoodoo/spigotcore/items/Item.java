78c7c7d9611b3e4283eabea77a061b7e1f804cdc
IkeVoodoo <55694349+IkeVoodoo@users.noreply.github.com>
2023-06-11 13:07:41 +0200

    Added getItemStackId

--- spigot-core-items/src/main/java/me/ikevoodoo/spigotcore/items/Item.java
+++ spigot-core-items/src/main/java/me/ikevoodoo/spigotcore/items/Item.java
@@ -4,7 +4,11 @@ import me.ikevoodoo.spigotcore.items.context.ClickContext;
 import me.ikevoodoo.spigotcore.items.context.SetupContext;
 import me.ikevoodoo.spigotcore.items.data.ItemData;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
@@ -13,8 +17,19 @@ import java.util.regex.Pattern;
 @SuppressWarnings("unused")
 public abstract class Item {
 
+    private static final NamespacedKey ID_KEY = new NamespacedKey(JavaPlugin.getProvidingPlugin(Item.class), "custom_item_key");
     private static final Pattern VARIABLE_PATTERN = Pattern.compile("\\$\\{(?<varname>[^}]+)}"); // TODO move this to a more specialized class?
     private final String id;
+
+    @Nullable
+    public static String getItemStackId(@NotNull ItemStack stack) {
+        Objects.requireNonNull(stack, "Cannot get item id from a null item stack!");
+
+        var meta = stack.getItemMeta();
+        Objects.requireNonNull(meta, "Cannot get item id from a null item meta!");
+
+        return meta.getPersistentDataContainer().get(ID_KEY, PersistentDataType.STRING);
+    }
 
     protected Item() {
         this.id = ItemRegistry.getId(this.getClass());
@@ -40,7 +55,10 @@ public abstract class Item {
             return null; // Unable to set up the item.
         }
 
-        var variables = new ItemVariables(meta.getPersistentDataContainer());
+        var pdc = meta.getPersistentDataContainer();
+        pdc.set(ID_KEY, PersistentDataType.STRING, this.getId());
+
+        var variables = new ItemVariables(pdc);
 
         this.setupItemStack(new SetupContext(variables));
 
