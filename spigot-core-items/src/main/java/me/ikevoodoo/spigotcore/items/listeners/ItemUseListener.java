0e050544207aa2d71407b4f6cb0b894ddd0a7978
IkeVoodoo <55694349+IkeVoodoo@users.noreply.github.com>
2023-06-11 13:08:03 +0200

    Filled in the player interact event

--- spigot-core-items/src/main/java/me/ikevoodoo/spigotcore/items/listeners/ItemUseListener.java
+++ spigot-core-items/src/main/java/me/ikevoodoo/spigotcore/items/listeners/ItemUseListener.java
@@ -1,5 +1,9 @@
 package me.ikevoodoo.spigotcore.items.listeners;
 
+import me.ikevoodoo.spigotcore.items.Item;
+import me.ikevoodoo.spigotcore.items.ItemRegistry;
+import me.ikevoodoo.spigotcore.items.ItemVariables;
+import me.ikevoodoo.spigotcore.items.context.ClickContext;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -12,6 +16,28 @@ public class ItemUseListener implements Listener {
 
     @EventHandler
     private void onPlayerInteract(PlayerInteractEvent event) {
+        var player = event.getPlayer();
+        var stack = event.getItem();
+        if (stack == null) return;
 
+        var meta = stack.getItemMeta();
+        if (meta == null) return;
+
+        var item = ItemRegistry.getInstance(stack);
+        if (item == null) {
+            var id = Item.getItemStackId(stack);
+            if (id == null) return; // This item has literally no id,
+            // How in a boundless hell full of tortured souls can we instantiate something with no id? Explain. Now.
+
+            item = ItemRegistry.createInstance(stack, id);
+        }
+
+        var variables = new ItemVariables(meta.getPersistentDataContainer());
+        var context = new ClickContext(player, variables, stack);
+
+        switch (event.getAction()) {
+            case LEFT_CLICK_AIR, LEFT_CLICK_BLOCK -> item.onLeftClick(context);
+            case RIGHT_CLICK_AIR ,RIGHT_CLICK_BLOCK -> item.onRightClick(context);
+        }
     }
 }
