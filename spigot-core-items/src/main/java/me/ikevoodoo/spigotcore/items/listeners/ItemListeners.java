fef1e191f5e1f81d1494d81564f5f6b94805e09b
IkeVoodoo <55694349+IkeVoodoo@users.noreply.github.com>
2023-06-11 12:44:18 +0200

    Setup item listeners w/ insults

---
+++ spigot-core-items/src/main/java/me/ikevoodoo/spigotcore/items/listeners/ItemListeners.java
@@ -0,0 +1,9 @@
+package me.ikevoodoo.spigotcore.items.listeners;
+
+public final class ItemListeners {
+
+    private ItemListeners() {
+        throw new IllegalStateException("Did you really just attempt this? The constructor is private for a reason you dingus.");
+    }
+
+}
