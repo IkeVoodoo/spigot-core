45c0dcf3665cb42a6b145976c05b617906d7851c
IkeVoodoo <55694349+IkeVoodoo@users.noreply.github.com>
2023-08-09 12:04:08 +0200

    Made comments have a default empty array

--- spigot-core-config/src/main/java/me/ikevoodoo/spigotcore/config/annotations/comments/Comments.java
+++ spigot-core-config/src/main/java/me/ikevoodoo/spigotcore/config/annotations/comments/Comments.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Comments {
 
-    String[] value();
+    String[] value() default {};
 
     String prefix() default " ";
 
