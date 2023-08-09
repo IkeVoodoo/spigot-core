8d29a2babfd82e8d6432ac223435f61c0b117595
IkeVoodoo <55694349+IkeVoodoo@users.noreply.github.com>
2023-08-09 12:04:56 +0200

    Comments can now be applied to classes Default array changed to array with empty string

--- spigot-core-config/src/main/java/me/ikevoodoo/spigotcore/config/annotations/comments/Comments.java
+++ spigot-core-config/src/main/java/me/ikevoodoo/spigotcore/config/annotations/comments/Comments.java
@@ -5,11 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Comments {
 
-    String[] value() default {};
+    String[] value() default { "" };
 
     String prefix() default " ";
 
